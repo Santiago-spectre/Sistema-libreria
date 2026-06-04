12fe57033f4f7a090c89c0d3d2501865b7e660cd
Santiago Lumba <santiago321lumbarojas456@gmail.com>
2026-06-04 00:13:39 -0500

    Actualización de Productos

---
+++ SistemaWebPapeleria/Models/Notification.cs
@@ -0,0 +1,15 @@
+﻿namespace SistemaWebPapeleria.Models
+{
+    public class Notification
+    {
+        public int NotificationId { get; set; }
+        public string Title { get; set; }
+        public string Message { get; set; }
+        public string Type { get; set; }
+        public bool IsRead { get; set; }
+        public string CreatedAt { get; set; }
+        
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
